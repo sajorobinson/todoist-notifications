6fe987cbeaae84880c454aedc57af80c90056c4b
Sam Robinson <sajorobinson@gmail.com>
2024-05-26 14:39:31 -0400

    Testing iterating through task objects

--- TodoistNotifications/Program.cs
+++ TodoistNotifications/Program.cs
@@ -4,8 +4,12 @@
     {
         public static async Task MainAsync()
         {
-            var result = Helpers.Json.DeserializeJson<Models.Task[]>(await Services.ApiCalls.GetActiveTasks());
-            Console.WriteLine(Helpers.Json.ReturnFirstOfDeserializedObject(result).Due?.Date);
+            var result = Helpers.Json.DeserializeJson<Models.Task[]>(await Services.Todoist.GetActiveTasks());
+            foreach (Models.Task task in result)
+            {
+                Console.WriteLine(task.Content);
+            }
+            //Console.WriteLine(Helpers.Json.ReturnFirstOfDeserializedObject(result).Due?.Date);
         }
         public static void Main()
         {
