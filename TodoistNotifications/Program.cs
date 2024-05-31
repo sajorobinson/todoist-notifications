bdc1c001afbce4cc3082e71ba6a05e6e981a81b3
Sam Robinson <sajorobinson@gmail.com>
2024-05-31 15:33:41 -0400

    Added logic for building the email payload by segmenting out the tasks by vicinity to the due time.

--- TodoistNotifications/Program.cs
+++ TodoistNotifications/Program.cs
@@ -5,7 +5,23 @@
         public static async Task MainAsync()
         {
             var result = Helpers.Json.DeserializeJson<Models.Task[]>(await Services.Todoist.GetActiveTasks());
-            string list = "";
+
+            Models.TaskList listDueNow = new Models.TaskList();
+            listDueNow.Title = "Due now:\n";
+            listDueNow.Items = "";
+
+            Models.TaskList listVeryUrgent = new Models.TaskList();
+            listVeryUrgent.Title = "Very urgent:\n";
+            listVeryUrgent.Items = "";
+
+            Models.TaskList listUrgent = new Models.TaskList();
+            listUrgent.Title = "Urgent: \n";
+            listUrgent.Items = "";
+
+            Models.TaskList listLessUrgent = new Models.TaskList();
+            listLessUrgent.Title = "Less urgent:\n";
+            listLessUrgent.Items = "";
+
             foreach (Models.Task task in result)
             {
                 if (task.Content is null)
@@ -22,27 +38,59 @@
                 }
                 else
                 {
-                    DateTime dueDate = Helpers.Time.ConvertDateStringToDateTime(task.Due?.DateTime);                    
-                    bool dueSoon = Helpers.Time.EvaluateDueDate(dueDate, 3);
-                    if (dueSoon)
+                    DateTime dueDate = Helpers.Time.ConvertDateStringToDateTime(task.Due?.DateTime!);
+                    
+                    bool dueNow = Helpers.Time.EvaluateDueDate(dueDate, Convert.ToInt32(Models.TaskUrgency.DueNow));
+                    bool veryUrgent = Helpers.Time.EvaluateDueDate(dueDate, Convert.ToInt32(Models.TaskUrgency.VeryUrgent));
+                    bool urgent = Helpers.Time.EvaluateDueDate(dueDate, Convert.ToInt32(Models.TaskUrgency.Urgent));
+                    bool lessUrgent = Helpers.Time.EvaluateDueDate(dueDate, Convert.ToInt32(Models.TaskUrgency.LessUrgent));
+                    
+                    if (dueNow)
                     {
-                        Console.WriteLine(task.Content + " -- " + task.Due.DateTime);
-                        list += task.Content.ToString() + "\n";
+                        listDueNow.Items += task.Content.ToString() + "\n";
+                    }
+                    else if (veryUrgent)
+                    {
+                        listVeryUrgent.Items += task.Content.ToString() + "\n";
+                    }
+                    else if (urgent)
+                    {
+                        listUrgent.Items += task.Content.ToString() + "\n";
+                    }
+                    else if (lessUrgent)
+                    {
+                        listUrgent.Items += task.Content.ToString() + "\n";
                     }
                     else
                     {
                         continue;
-                    } 
+                    }
                 }
             }
-            if (list.Length > 0)
+
+            string payload = "";
+
+            if (listDueNow.Items.Length > 0)
             {
-                // await Services.SendGrid.SendEmail("This is a test", list, "");
+                payload += listDueNow.Title + "\n" + listDueNow.Items + "\n";
             }
-            else
+
+            if (listVeryUrgent.Items.Length > 0)
             {
-                Console.WriteLine("No tasks due.");
+                payload += listVeryUrgent.Title + "\n" + listVeryUrgent.Items + "\n";
             }
+
+            if (listUrgent.Items.Length > 0)
+            {
+                payload += listUrgent.Title + "\n" + listUrgent.Items + "\n";
+            }
+
+            if (listLessUrgent.Items.Length > 0)
+            {
+                payload += listLessUrgent.Title + "\n" + listUrgent.Items + "\n";
+            }
+
+            await Services.SendGrid.SendEmail("This is a test", payload, "");
         }
         public static void Main()
         {
