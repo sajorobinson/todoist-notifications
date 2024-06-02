6ea2e5d76af01da5d385b291598d39f69f2d25ba
Sam Robinson <sajorobinson@gmail.com>
2024-06-02 10:25:21 -0500

    Modified task urgency descriptions to be more accurate

--- TodoistNotifications/Models/TaskUrgency.cs
+++ TodoistNotifications/Models/TaskUrgency.cs
@@ -6,12 +6,12 @@ namespace Models
         public static readonly string DueNowTitle = "Due now";
 
         public static readonly int VeryUrgent = 1;
-        public static readonly string VeryUrgentTitle = "Due in an hour";
+        public static readonly string VeryUrgentTitle = "Due in next hour";
 
         public static readonly int Urgent = 8;
-        public static readonly string UrgentTitle = "Due in eight hours";
+        public static readonly string UrgentTitle = "Due in next eight hours";
 
         public static readonly int LessUrgent = 24;
-        public static readonly string LessUrgentTitle = "Due in 24 hours";
+        public static readonly string LessUrgentTitle = "Due in next 24 hours";
     }
 }
