3516a82eaa45c1728eea85c39612499d2510f470
Sam Robinson <sajorobinson@gmail.com>
2024-06-11 11:17:01 -0400

    Switched to using StringBuilder() for payload formation instead of concatenation

--- TodoistNotifications/Models/TaskList.cs
+++ TodoistNotifications/Models/TaskList.cs
@@ -1,8 +1,10 @@
+using System.Text;
+
 namespace Models
 {
     public class TaskList
     {
         public string? Title { get; set; }
-        public string? Items { get; set; }
+        public StringBuilder? Items { get; set; }
     }
 }
--- TodoistNotifications/Program.cs
+++ TodoistNotifications/Program.cs
@@ -1,89 +1,80 @@
-﻿namespace Program
+﻿using System.Text;
+
+namespace Program
 {
     public class Program
     {
         public static async Task MainAsync()
         {
             var result = Helpers.Json.DeserializeJson<Models.Task[]>(await Services.Todoist.GetActiveTasks());
-
             Models.TaskList dueNowList = new Models.TaskList
             {
                 Title = Models.TaskUrgency.DueNowTitle,
-                Items = ""
+                Items = new StringBuilder()
             };
-
             Models.TaskList veryUrgentList = new Models.TaskList
             {
                 Title = Models.TaskUrgency.VeryUrgentTitle,
-                Items = ""
+                Items = new StringBuilder()
             };
-
             Models.TaskList urgentList = new Models.TaskList
             {
                 Title = Models.TaskUrgency.UrgentTitle,
-                Items = ""
+                Items = new StringBuilder()
             };
-
             Models.TaskList lessUrgentList = new Models.TaskList
             {
                 Title = Models.TaskUrgency.LessUrgentTitle,
-                Items = ""
+                Items = new StringBuilder()
             };
-
             foreach (Models.Task task in result)
             {
-                if (task.Content is null | task.Due is null | task.Due?.DateTime is null)
+                if (task.Content is null || task.Due is null || task.Due?.DateTime is null)
                 { 
                     continue;
                 }
+                DateTime dueDate = Helpers.Time.ConvertDateStringToDateTime(task.Due.DateTime);
+                double dueIn = Helpers.Time.EvaluateDueDate(dueDate);
+                string taskContent = task.Content.ToString() + "\n";
+                if (dueIn <= Models.TaskUrgency.DueNow)
+                {
+                    dueNowList.Items.Append(taskContent);
+                }
+                else if (dueIn <= Models.TaskUrgency.VeryUrgent)
+                {
+                    veryUrgentList.Items.Append(taskContent);
+                }
+                else if (dueIn <= Models.TaskUrgency.Urgent)
+                {
+                    urgentList.Items.Append(taskContent);
+                }
+                else if (dueIn <= Models.TaskUrgency.LessUrgent)
+                {
+                    lessUrgentList.Items.Append(taskContent);
+                }
                 else
                 {
-                    DateTime dueDate = Helpers.Time.ConvertDateStringToDateTime(task.Due?.DateTime!);
-
-                    double dueIn = Helpers.Time.EvaluateDueDate(dueDate);
-
-                    if (dueIn <= Models.TaskUrgency.DueNow)
-                    {
-                        dueNowList.Items += task.Content!.ToString() + "\n";
-                    }
-                    else if (dueIn <= Models.TaskUrgency.VeryUrgent)
-                    {
-                        veryUrgentList.Items += task.Content!.ToString() + "\n";
-                    }
-                    else if (dueIn <= Models.TaskUrgency.Urgent)
-                    {
-                        urgentList.Items += task.Content!.ToString() + "\n";
-                    }
-                    else if (dueIn <= Models.TaskUrgency.LessUrgent)
-                    {
-                        lessUrgentList.Items += task.Content!.ToString() + "\n";
-                    }
-                    else
-                    {
-                        continue;
-                    }
+                    continue;
                 }
             }
-
-            string payload = "";
+            StringBuilder payload = new StringBuilder();
             if (dueNowList.Items.Length > 0)
             {
-                payload += dueNowList.Title + "\n" + dueNowList.Items + "\n";
+                payload.Append($"{dueNowList.Title}\n{dueNowList.Items}\n");
             }
             if (veryUrgentList.Items.Length > 0)
             {
-                payload += veryUrgentList.Title + "\n" + veryUrgentList.Items + "\n";
+                payload.Append($"{veryUrgentList.Title}\n{veryUrgentList.Items}\n");
             }
             if (urgentList.Items.Length > 0)
             {
-                payload += urgentList.Title + "\n" + urgentList.Items + "\n";
+                payload.Append($"{urgentList.Title}\n{urgentList.Items}\n");
             }
             if (lessUrgentList.Items.Length > 0)
             {
-                payload += lessUrgentList.Title + "\n" + urgentList.Items + "\n";
+                payload.Append($"{lessUrgentList.Title}\n{lessUrgentList.Items}\n");
             }
-
-            await Services.SendGrid.SendEmail("This is a test", payload, "");
+            await Services.SendGrid.SendEmail("This is a test", payload.ToString(), "");
         }
         public static void Main()
         {
